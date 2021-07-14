3b63af69361597bea9e9fffb9ba7c72537f271b3
bonniegee <bgordon@rockarch.org>
2021-07-14 17:34:19 -0400

    add placeholder methods

--- create_bags/bag_creator.py
+++ create_bags/bag_creator.py
@@ -1,18 +1,25 @@
 import logging
-
 from configparser import ConfigParser
-from .archivesspace import ArchivesSpaceClient
+
+# from .archivesspace import ArchivesSpaceClient
 
 
 class BagCreator():
-    """docstring for BagCreator"""
 
     def __init__(self):
         logging.basicConfig(
             datefmt='%m/%d/%Y %I:%M:%S %p',
-            filename='iiif_preparer.log',
+            filename='bag_creator.log',
             format='%(asctime)s %(message)s',
             level=logging.INFO)
         self.config = ConfigParser()
         self.config.read("local_settings.cfg")
         self.ignore_list = self.config.get("IgnoreList", "ignore_list")
+
+    def create_target_directory(self):
+        """docstring for create_target_directory"""
+    pass
+
+    def copy_files_to_target_directory(self):
+        """docstring for copy_files_to_target_directory"""
+    pass
