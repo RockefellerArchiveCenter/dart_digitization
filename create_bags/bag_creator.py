4482c754b5a2caf1a5e3725ac4c669209c954435
bonniegee <bgordon@rockarch.org>
2021-08-27 17:28:33 -0400

    clean up variable assignment, add rights info

--- create_bags/bag_creator.py
+++ create_bags/bag_creator.py
@@ -19,10 +19,6 @@ class BagCreator:
         self.config.read("local_settings.cfg")
         self.dest_location = self.config.get("Locations", "dest_location")
         # TODO: we should alias dart? where are we installing dart?
-        self.as_client = ArchivesSpaceClient(baseurl=self.config.get(
-            "ArchivesSpace", "baseurl"), username=self.config.get(
-            "ArchivesSpace", "username"), password=self.config.get(
-            "ArchivesSpace", "password"))
         self.dart_command = "we gotta figure this out"
 
     def run(self, top_dir, refid, rights_ids, files):
@@ -32,39 +28,42 @@ class BagCreator:
         rights_ids (array)
         """
         # directory_to_bag = "some directory"
+        self.as_client = ArchivesSpaceClient(baseurl=self.config.get(
+            "ArchivesSpace", "baseurl"), username=self.config.get(
+            "ArchivesSpace", "username"), password=self.config.get(
+            "ArchivesSpace", "password"))
         self.refid = refid
-        self.rights_ids = rights_ids
-        self.files = files
         self.ao_uri = self.as_client.get_ao_uri(self.refid)
         ao_data = self.as_client.get_ao_data(self.ao_uri)
-        dates = get_dates(ao_data)
-        self.start_date, self.end_date = format_aspace_date(dates)
-        self.job_params = self.construct_job_params()
+        dates = format_aspace_date(get_dates(ao_data))
+        self.job_params = self.construct_job_params(rights_ids, files, dates)
         self.create_dart_job()
         # TODO: return path to created bag, any message from DART
         return self.refid
 
-    def construct_job_params(self):
+    def construct_job_params(self, rights_ids, files, dates):
         """Formats information for DART job parameters
 
         Returns a dictionary"""
         job_params = {"workflowName": "Digitization Workflow"}
         job_params['packageName'] = "{}.tar".format(self.refid)
-        job_params['files'] = self.files
+        job_params['files'] = files
         tags = []
         tags.append(create_tag("ArchivesSpace-URI", self.ao_uri))
-        tags.append(create_tag("Start-Date", self.start_date))
-        tags.append(create_tag("End-Date", self.end_date))
+        tags.append(create_tag("Start-Date", dates[0]))
+        tags.append(create_tag("End-Date", dates[1]))
         tags.append(create_tag("Origin", "digitization"))
+        for rights_id in rights_ids:
+            tags.append(create_tag("Rights-ID", rights_id))
         job_params['tags'] = tags
         return job_params
 
     def create_dart_job(self):
         """Runs a DART job"""
-        json_string = json.dumps(self.job_params)
+        json_input = (json.dumps(self.job_params) + "\n").encode()
         cmd = "{} -- --stdin".format(self.dart_command)
         child = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, close_fds=True)
-        stdout_data, stderr_data = child.communicate(json_string + "\n")
+        stdout_data, stderr_data = child.communicate(json_input)
         if stdout_data is not None:
             raise Exception(stdout_data)
         elif stderr_data is not None:
--- tests/test_create.py
+++ tests/test_create.py
@@ -6,15 +6,15 @@ def test_construct_job_params():
     bag_creator = BagCreator()
     bag_creator.refid = "jsdfjp90fsfjlk"
     bag_creator.ao_uri = "/whatever"
-    bag_creator.start_date = "1940-01-01"
-    bag_creator.end_date = "1940-06-01"
-    bag_creator.files = ["/path/to/file1.tif", "/path/to/file2.tif"]
-    job_params = bag_creator.construct_job_params()
+    rights_ids = [2, 4]
+    dates = ("1940-01-01", "1940-06-01")
+    files = ["/path/to/file1.tif", "/path/to/file2.tif"]
+    job_params = bag_creator.construct_job_params(rights_ids, files, dates)
     assert isinstance(job_params, dict)
     for tag in job_params["tags"]:
         if tag["tagName"] == "Start-Date":
             assert tag["userValue"] == "1940-01-01"
-    assert len(job_params["tags"]) == 4
+    assert len(job_params["tags"]) == 6
 
 
 # test_construct_job_params
