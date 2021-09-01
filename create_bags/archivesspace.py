6b149b2207b68676621572a6c091f5a79a1b4c51
bonniegee <bgordon@rockarch.org>
2021-09-01 19:01:48 -0400

    better function name, variable handling

--- create_bags/archivesspace.py
+++ create_bags/archivesspace.py
@@ -8,7 +8,7 @@ class ArchivesSpaceClient:
             username=username,
             password=password)
 
-    def get_ao_uri(self, refid):
+    def get_uri_from_refid(self, refid):
         """Use find_by_refid endpoint to return the URI of an archival object"""
         find_by_refid_url = "repositories/2/find_by_id/archival_objects?ref_id[]={}".format(
             refid)
@@ -21,7 +21,6 @@ class ArchivesSpaceClient:
 
     def get_ao_data(self, ao_uri):
         """Gets data for an archival object, including ancestors"""
-        ao_json = self.client.get(
+        return self.client.get(
             ao_uri, params={
                 "resolve": ["ancestors"]}).json()
-        return ao_json
