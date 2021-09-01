7aa6cb609b2d983890e48396ef7a571971655d35
bonniegee <bgordon@rockarch.org>
2021-09-01 18:57:10 -0400

    handle unexpected refid search result

--- create_bags/archivesspace.py
+++ create_bags/archivesspace.py
@@ -13,7 +13,11 @@ class ArchivesSpaceClient:
         find_by_refid_url = "repositories/2/find_by_id/archival_objects?ref_id[]={}".format(
             refid)
         results = self.client.get(find_by_refid_url).json()
-        return results['archival_objects'][0]['ref']
+        if len(results.get("archival_objects")) == 1:
+            return results['archival_objects'][0]['ref']
+        else:
+            raise Exception("{} results found for search {}".format(
+                len(results.get("archival_objects")), find_by_refid_url))
 
     def get_ao_data(self, ao_uri):
         """Gets data for an archival object, including ancestors"""
