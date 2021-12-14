beca30605ef7e42bc04ddb6bc35a594adc596022
bonniegee <bgordon@rockarch.org>
2021-12-13 19:23:25 -0500

    simplify variable assignment

--- create_bags/bag_creator.py
+++ create_bags/bag_creator.py
@@ -70,12 +70,6 @@ class BagCreator:
         child = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, close_fds=True)
         stdout_data, stderr_data = child.communicate(json_input)
         if child.returncode != 0:
-            if stdout_data:
-                stdout_message = stdout_data.decode('utf-8')
-            else:
-                stdout_message = ""
-            if stderr_data:
-                stderr_message = stderr_data.decode('utf-8')
-            else:
-                stderr_message = ""
+            stdout_message = stdout_data.decode('utf-8') if stdout_data else ""
+            stderr_message = stderr_data.decode('utf-8') if stderr_data else ""
             raise Exception(stdout_message, stderr_message)
